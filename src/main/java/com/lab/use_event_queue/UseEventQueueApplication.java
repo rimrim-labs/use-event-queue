b6460ed7047d0945a751644c333c0527d34de6bc
rimrim990 <lim990718@naver.com>
2023-12-19 21:42:14 +0900

    init

---
+++ src/main/java/com/lab/use_event_queue/UseEventQueueApplication.java
@@ -0,0 +1,13 @@
+package com.lab.use_event_queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseEventQueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UseEventQueueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lab/use_event_queue/UseEventQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.use_event_queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseEventQueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
